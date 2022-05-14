cd5c3c80ba0bc60a4e301d8202ed8c3f18badb2e
fcastano <fcastano@lifefile.net>
2022-05-13 21:52:30 -0500

    Add entities

---
+++ core/content_mng_api/core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.db.models import Model
+
+class Category(Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+class Tag(Model):
+    title = models.CharField(max_length=100)
+
+class Content(Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    image = models.URLField()
+    dealine = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
